d506be8a364dd713146e17657dacfa927b411c61
Doreen <Doreen@DESKTOP-S59AJ7A>
2018-12-31 14:48:49 +0200

    Doreen. advanced search connection done.

--- src/GUI/AdvancedSearchController.java
+++ src/GUI/AdvancedSearchController.java
@@ -4,16 +4,18 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdvancedSearchController {
     @FXML
@@ -32,6 +34,17 @@ public class AdvancedSearchController {
     private Button advanced;
     @FXML
     private Pane genres;
+    @FXML
+    private Slider tempo;
+    @FXML
+    private Slider hotness;
+    @FXML
+    private Slider duration;
+    @FXML
+    private TextField artist_name;
+    @FXML
+    private TextField album_name;
+
 
     protected void init(String era, ObservableList list, String birthYear, String age) {
         this.era.setValue(era);
@@ -74,6 +87,107 @@ public class AdvancedSearchController {
             searchController.initialize(era.getValue(), genres.getChildren(), birthYear.getText(), age.getText());
             scene.getStylesheets().add(getClass().getResource("SearchCss.css").toExternalForm());
             stage.setTitle("Search");
+            stage.setScene(scene);
+            stage.show();
+            setCenter(stage);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addValues(Map<String, ArrayList<String>> map ){
+        if (era.isDisable() == false){
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add(era.getValue());
+            map.put("era",arr);
+        }
+        else{
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add(birthYear.getText());
+            map.put("birthYear",arr);
+            arr = new ArrayList<>();
+            arr.add(age.getText());
+            map.put("age",arr);
+        }
+        ArrayList<String> arr = new ArrayList<>();
+        for (Node genre :genres.getChildren()){
+            if(((CheckBox)genre).isSelected()){
+                String g = ((CheckBox) genre).getText();
+                arr.add(g);
+            }
+        }
+        map.put("genre",arr);
+        //adding tempo
+        arr.clear();
+        arr.add(Double.toString(tempo.getValue()));
+        map.put("tempo",arr);
+
+        //adding hotness
+        arr.clear();
+        arr.add(Double.toString(hotness.getValue()));
+        map.put("hotness",arr);
+
+        //adding duration
+        arr.clear();
+        arr.add(Double.toString(duration.getValue()));
+        map.put("duration",arr);
+
+        //adding artist_name
+        arr.clear();
+        arr.add(artist_name.getText());
+        map.put("artist_name",arr);
+
+        //adding album_name
+        arr.clear();
+        arr.add(album_name.getText());
+        map.put("album_name",arr);
+
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private boolean checkValues(){
+        if (era.isDisable() == true){
+            String birthYear = this.birthYear.getText();
+            String age = this.age.getText();
+            if( birthYear.equals("") || Integer.parseInt(birthYear) > 2018 || Integer.parseInt(birthYear) < 1900){
+                showAlert("birth year is invalid");
+                return false;
+            }
+            if( age.equals("") || Integer.parseInt(age)<1 || Integer.parseInt(age)>120 ){
+                showAlert("age is invalid");
+                return false;
+            }
+            if(Integer.parseInt(birthYear)+Integer.parseInt(age)>2019){
+                showAlert("birth year or age is incorrect");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @FXML
+    protected void results() {
+        try {
+            if(checkValues() == false)
+                return;
+            Connection connection = Connection.getInstance();
+            Map<String, ArrayList<String>> map = new HashMap<>();
+            addValues(map);
+            for (String key:map.keySet()) {
+                System.out.println(key+" "+map.get(key));
+            }
+            connection.query(map);
+
+            Stage stage = (Stage) results.getScene().getWindow();
+            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Results.fxml"));
+            Scene scene = new Scene(root,450,500);
+            stage.setTitle("Nostalgia");
             stage.setScene(scene);
             stage.show();
             setCenter(stage);
